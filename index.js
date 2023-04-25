6a25677f0a2ede0118640ec2b0747b28120e6557
Manas <uniyalmanas@gmail.com>
2023-04-25 18:50:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const router = require('./router')
+app.use(router)
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
